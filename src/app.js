b8c0d991d50fa3205ec62fd7286550de1f70862d
Brunna Serafina <brunnaserafina@gmail.com>
2022-09-08 23:48:21 -0300

    feat: validating login and returning token in route - POST '/sign-in'

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { Mongo, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
@@ -16,5 +16,35 @@ mongoClient.connect().then(() => {
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+server.post("/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await db.collection("users").findOne({ email });
+
+    if (!user) {
+      return res.status(401).send("Usuário ou senha incorretas");
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(401).send("Usuário ou senha incorretas");
+    }
+
+    const token = uuid();
+
+    await db.collection("sessions").insertOne({
+      userId: user._id,
+      token,
+    });
+
+    return res.send(token);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+});
 
 server.listen(5000, () => console.log("Listening on port 5000"));
